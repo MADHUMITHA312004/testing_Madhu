6182d51205e20bb3721115a9daf804e416c2ffbf
Madhumitha M <124856031+MADHUMITHA312004@users.noreply.github.com>
2024-04-22 22:41:42 +0530

    Add files via upload

---
+++ software testing/day4(1)/day4ex1/src/test/java/com/example/day4ex1/Day4ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day4ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day4ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
